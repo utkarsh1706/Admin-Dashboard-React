136ef2e4ea752d1571b28f4841c03f63fd3d1867
Utkarsh Kuchhal <kuchhalutkarsh17@gmail.com>
2024-04-28 23:50:51 +0530

    Sidebar and Topbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import { useState } from "react";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 function App() {
   const [theme, colorMode] = useMode();
-
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+        <Sidebar isSidebar={isSidebar}/>
           <main className="content"></main>
+          <Topbar setIsSidebar={setIsSidebar} />
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
